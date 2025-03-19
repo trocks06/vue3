1d68938aa2c44baeb7d1daf9892e6adccea57e43
gr422_hadal <denishatkov17@gmail.com>
2025-03-19 18:48:14 +0700

    Оформление данных карточки

--- js/main.js
+++ js/main.js
@@ -15,59 +15,65 @@ Vue.component('to-do-list', {
                     <h3>{{ card.name }}</h3>
                     <button class="card-button">Вправо</button>
                 </div>
+                <div class="card-body">
+                    <div class="card-info">
+                        <h4>Задание</h4>
+                        <p>{{ card.task }}</p>
+                    </div>
+                    <div class="card-info">
+                        <h4>Дата создания</h4>
+                        <p>{{ card.createdData }}</p>
+                    </div>
+                    <div class="card-info">
+                        <h4>Срок выполнения</h4>
+                        <p>{{ card.deadline }}</p>
+                    </div>                                        
+                </div>
             </div>
         </div>
         <div class="column second-column">
             <h1>Задачи в работе</h1>
             <div v-for="(card, index) in column2" :key="index" class="content">
-                <h3>{{ card.name }}</h3>
-                <ul class="no-marker">
-                    <li v-for="(task, taskIndex) in card.tasks" :key="taskIndex">
-                        <input type="checkbox" 
-                               :checked="task.taskCompleted"
-                               @change="toggleTaskCompletion(card, task, 'column2', index)">
-                        {{ task.taskName }}
-                    </li>
-                </ul>
+                <div class="card-head">
+                    <button class="card-button">Влево</button>
+                    <h3>{{ card.name }}</h3>
+                    <button class="card-button">Вправо</button>
+                </div>
+                <div class="card-body">
+                    <p>Задание: {{ card.task }}</p>
+                    <p>Дата создания: {{ card.createdData }}</p>
+                    <p>Срок выполнения: {{ card.deadline }}</p>
+                </div>
             </div>
         </div>
         <div class="column third-column">
             <h1>Тестирование</h1>
             <div v-for="(card, index) in column3" :key="index" class="content">
-                <h3>{{ card.name }}</h3>
-                <ul class="no-marker">
-                    <li v-for="(task, taskIndex) in card.tasks" :key="taskIndex">
-                        <input disabled type="checkbox" 
-                               :checked="task.taskCompleted"
-                               @change="toggleTaskCompletion(card, task, 'column3', index)">
-                        {{ task.taskName }}
-                    </li>
-                </ul>
-                <p>Дата выполнения: {{ card.completedDate }}</p>
+                <div class="card-head">
+                    <button class="card-button">Влево</button>
+                    <h3>{{ card.name }}</h3>
+                    <button class="card-button">Вправо</button>
+                </div>
+                <p>{{ card.task }}</p>
+                <p>{{ card.createdData }}</p>
+                <p>{{ card.deadline }}</p>
             </div>
         </div>
         <div class="column fourth-column">
             <h1>Выполненные задачи</h1>
-            <div v-for="(card, index) in column3" :key="index" class="content">
-                <h3>{{ card.name }}</h3>
-                <ul class="no-marker">
-                    <li v-for="(task, taskIndex) in card.tasks" :key="taskIndex">
-                        <input disabled type="checkbox" 
-                               :checked="task.taskCompleted"
-                               @change="toggleTaskCompletion(card, task, 'column4', index)">
-                        {{ task.taskName }}
-                    </li>
-                </ul>
-                <p>Дата выполнения: {{ card.completedDate }}</p>
+            <div v-for="(card, index) in column4" :key="index" class="content">
+                <div class="card-head">
+                    <button class="card-button">Влево</button>
+                    <h3>{{ card.name }}</h3>
+                    <button class="card-button">Вправо</button>
+                </div>
+                <p>{{ card.task }}</p>
+                <p>{{ card.createdData }}</p>
+                <p>{{ card.deadline }}</p>
             </div>
         </div>
     </div>
     `,
-    data() {
-        return {
-            checkboxDisabled: Number(JSON.parse(localStorage.getItem('checkboxDisabled'))) || 0,
-        };
-    },
     methods: {
         toggleTaskCompletion(card, task, column, index) {
             task.taskCompleted = !task.taskCompleted;
@@ -75,51 +81,12 @@ Vue.component('to-do-list', {
             this.checkTaskInSecondColumn();
             this.updateLocalStorage();
         },
-        checkTaskComplete(card, index, column) {
-            const tasks = card.tasks.length;
-            const completedTasks = card.tasks.filter(task => task.taskCompleted).length;
-
-            if (column === 'column1' && completedTasks >= tasks * 0.5) {
-                if (this.column2.length < 5) {
-                    this.checkboxDisabled = 0;
-                    this.$emit('move-to-column2', card, index, column);
-                } else {
-                    this.checkboxDisabled = 1;
-                }
-            } else if (column === 'column2') {
-                if (completedTasks < tasks * 0.5) {
-                    this.$emit('move-to-column1', card, index);
-                } else if (completedTasks === tasks) {
-                    this.$emit('move-to-column3', card, index);
-                    card.completedDate = new Date();
-                    card.completedDate = card.completedDate.toLocaleString()
-                }
-            } else if (column === 'column3' && completedTasks !== tasks) {
-                this.$emit('move-to-column2', card, index, column);
-            }
-        },
-        checkTaskInFirstColumn(card, index) {
-            for (let i = 0; i < this.column1.length; i++) {
-                const tasks = this.column1[i].tasks.length;
-                const completedTasks = this.column1[i].tasks.filter(task => task.taskCompleted).length;
-                if (completedTasks >= tasks * 0.5) {
-                    this.checkTaskComplete(card, i, "column1")
-                }
-            }
-        },
-        checkTaskInSecondColumn() {
-            if (this.column2.length < 5) {
-                this.checkboxDisabled = 0;
-                for (let i = 0; i < this.column1.length; i++) {
-                    this.checkTaskComplete(this.column1[i], i, "column1");
-                }
-            }
-        },
         updateLocalStorage() {
             localStorage.setItem('checkboxDisabled', JSON.stringify(this.checkboxDisabled));
             localStorage.setItem('column1', JSON.stringify(this.column1));
             localStorage.setItem('column2', JSON.stringify(this.column2));
             localStorage.setItem('column3', JSON.stringify(this.column3));
+            localStorage.setItem('column3', JSON.stringify(this.column4));
         }
     }
 });
@@ -141,19 +108,19 @@ Vue.component('card-create', {
             </ul>
             <div class="form-div">
                 <label for="name">Название карточки</label>
-                <input name="name" type="text" placeholder="Название" v-model="name">
+                <input id="name" type="text" placeholder="Название" v-model="name">
             </div>
             <div class="form-div">
-            <label for="task">Задание</label>
-            <input name="task"  type="text" placeholder="Задание" v-model="task">
+                <label for="task">Задание</label>
+                <input id="task"  type="text" placeholder="Задание" v-model="task">
             </div>
             <div class="form-div">
-            <label for="deadline">Срок выполнения</label>
-            <input name="deadline"  type="text" placeholder="Срок" v-model="deadline">
+                <label for="deadline">Срок выполнения</label>
+                <input id="deadline"  type="text" placeholder="Срок" v-model="deadline">
             </div>
             <div class="form-div-buttons">
                 <input type="submit" value="Создать">
-                <button type="button" @click="closeModal">Закрыть</button>
+                <button class="modal-close-button" type="button" @click="closeModal">Закрыть</button>
             </div>
         </form>
     </div>
@@ -165,7 +132,6 @@ Vue.component('card-create', {
             createdData: '',
             deadline: '',
             errors: [],
-            inputs: 3
         }
     },
     methods: {
@@ -186,35 +152,27 @@ Vue.component('card-create', {
             if (!this.deadline) {
                 this.errors.push("Добавьте дедлайн");
             }
-            if (this.name && this.newTasks.length >= 3 && this.newTasks.length <= 5) {
+            if (this.name && this.task && this.deadline) {
                 let card = {
                     name: this.name,
                     task: this.task,
                     createdData: this.createdData,
-
+                    deadline: this.deadline
                 };
                 this.$emit('add-card', card);
                 this.closeModal();
                 this.name = '';
-                this.newTasks = [];
-                this.inputs = 3;
-            }
-        },
-        addTask() {
-            this.errors = [];
-            if (this.inputs >= 5) {
-                this.errors.push("Больше пяти заданий сделать нельзя");
-            } else {
-                this.inputs++;
-                this.newTasks.push('');
+                this.task = '';
+                this.createdData = '';
+                this.deadline = '';
             }
         },
         closeModal() {
             this.$emit('modal-close');
             this.name = '';
-            this.newTasks = [];
-            this.inputs = 3;
-            this.errors = [];
+            this.task = '';
+            this.createdData = '';
+            this.deadline = '';
         },
     }
 });
@@ -263,6 +221,7 @@ let app = new Vue({
             localStorage.setItem('column1', JSON.stringify(this.column1));
             localStorage.setItem('column2', JSON.stringify(this.column2));
             localStorage.setItem('column3', JSON.stringify(this.column3));
+            localStorage.setItem('column3', JSON.stringify(this.column4));
         },
     },
 })
